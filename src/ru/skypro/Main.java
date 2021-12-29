aaf4fcb4415e6e4dd5ef397cf485ebd4b5e787af
Антон <antonL1790@yandex.ru>
2021-12-29 20:50:13 +0300

    HW №4 ready

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,48 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        task1();
+        task2();
+        task3();
     }
+
+    public static void task1() {
+
+
+        for (int i = 1; i < 11; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+
+        int i = 11;
+        while (i > 1) {
+            i--;
+            System.out.print(" " + i);
+        }
+        System.out.println();
+    }
+
+
+    public static void task2() {
+
+
+        for (int i = 3; i < 32; i += 7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+    }
+
+
+    public static void task3() {
+
+        int ourPeriod = 200;
+        int nextPeriod = 100;
+        int total = ourPeriod + nextPeriod;
+
+        for (int i = 0; i < total; i += 79) {
+            System.out.println(i);
+        }
+    }
+
 }
+
